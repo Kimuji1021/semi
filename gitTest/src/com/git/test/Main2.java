bc6f1c0754dc21f2ea84aa0ce5241f83fac6f6e3
user1 <user1@user1-PC>
2018-12-19 14:55:04 +0900

    test commit

--- gitTest/src/com/git/test/Main2.java
+++ gitTest/src/com/git/test/Main2.java
@@ -4,6 +4,17 @@ public class Main2 {
 	
 	private int age;
 	private String hobby;
+	private String address;
+	private String name;
+	
+	public String getAddress()
+	{
+		return address;
+	}
+	public String getName()
+	{
+		return name;
+	}
 	
 	public String getHobby()
 	{
